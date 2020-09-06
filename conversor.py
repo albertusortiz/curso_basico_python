b94041c1e272ebcb04c39787fe1bcac086c1ef71
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2020-09-06 00:28:06 -0500

    Creando algoritmo para convertir pesos colombianos a dolares

---
+++ conversor.py
@@ -0,0 +1,6 @@
+pesos = input("Cuantos pesos colombianos tienes?: ")
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = str(pesos / valor_dolar)
+
+print(f"Tus {pesos} colombianos equivalen a {dolares} dolares.")
